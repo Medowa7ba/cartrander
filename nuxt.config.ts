cae3998ac2a293471173fff984c071a2577fbbdb
Mohamed <baianat@Mohameds-iMac.local>
2024-08-21 11:39:59 +0300

    add pageTransition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge", "@vueuse/nuxt", '@nuxtjs/supabase'],
-
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
 });
