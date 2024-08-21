4dd038ae2bf8291fb1feff2bc80f3ad50ad772d4
Mohamed <baianat@Mohameds-iMac.local>
2024-08-21 11:51:34 +0300

    lottie animation

--- error.vue
+++ error.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
+        <Vue3Lottie
+      animationLink="https://assets10.lottiefiles.com/packages/lf20_soCRuE.json"
+      :height="200"
+      :width="200"
+    />
         <h1 class="text-9xl">{{error.statusCode}}</h1>
         <p class="mt-7 text-4xl">{{error.message}}</p>
         <button @click="handleError" class="rounded mt-7 text-2xl bg-blue-400 px-7 py-4 text-white">Go Back</button>
@@ -7,6 +12,7 @@
 </template>
 
 <script lang="ts" setup>
+import { Vue3Lottie } from 'vue3-lottie'
 const handleError = () => {
     clearError({
       redirect: '/'
